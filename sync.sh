d5e20d09d1e6ea6d0a6ad6e76a0b958204848f50
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2016-12-29 13:38:12 +0100

    Switch to LineageOS
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ..
-mkdir CM14.1
-cd CM14.1
-repo init -u https://github.com/CyanogenMod/android.git -b cm-14.1
+mkdir LineageOS
+cd LineageOS
+repo init -u https://github.com/LineageOS/android.git -b cm-14.1
 mkdir .repo/local_manifests && cd .repo/local_manifests
-wget https://raw.githubusercontent.com/Nougat-w7/local_manifests/master/cm-14.1.xml
+wget https://raw.githubusercontent.com/Nougat-w7/local_manifests/master/lineage-14.1.xml
 cd ../..
 repo sync --force-sync -j5
