706abb78145f3d9105071a92de2ca0cdabd03469
GrinningFerret <luis.hage@outlook.de>
2017-01-09 07:14:58 +0100

    Improve script
    Signed-off-by: GrinningFerret <luis.hage@outlook.de>

--- sync.sh
+++ sync.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-cd ..
-mkdir LineageOS
-cd LineageOS
+mkdir ~/LineageOS
+cd ~/LineageOS
 repo init -u https://github.com/LineageOS/android.git -b cm-14.1
-mkdir .repo/local_manifests && cd .repo/local_manifests
-wget https://raw.githubusercontent.com/Nougat-w7/local_manifests/master/lineage-14.1.xml
-cd ../..
-repo sync --force-sync -j5
+curl https://raw.githubusercontent.com/Nougat-w7/local_manifests/master/lineage-14.1.xml --create-dirs -o ~/LineageOS/.repo/local_manifests/lineage-14.1.xml
+repo sync -f --force-sync -j5
